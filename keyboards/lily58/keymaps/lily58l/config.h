7c47bf5b429b3c8adae11e9edfef6291e13e3097
Jeremy Laskar <jeremy.laskar@fictiv.com>
2023-02-19 19:10:46 -1000

    webrender config

--- keyboards/lily58/keymaps/lily58l/config.h
+++ keyboards/lily58/keymaps/lily58l/config.h
@@ -22,8 +22,8 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #pragma once
 
 /* Select hand configuration */
-#define MASTER_LEFT
-// #define MASTER_RIGHT
+// #define MASTER_LEFT
+#define MASTER_RIGHT
 // #define EE_HANDS
 
 #ifdef RGBLIGHT_ENABLE
--- keyboards/lily58/light/rules.mk
+++ keyboards/lily58/light/rules.mk
@@ -1,3 +1,11 @@
 ENCODER_ENABLE = yes       # ENables the use of one or more encoders
 RGBLIGHT_ENABLE = yes      # Enable keyboard RGB light
 LTO_ENABLE = yes 		   # significantly reduce the compiled size, but disable the legacy TMK Macros and Functions features
+OLED_ENABLE = yes
+OLED_DRIVER = SSD1306 
+EXTRAKEY_ENABLE = yes
+EXTRAFLAGS += -flto
+POINTING_DEVICE_ENABLE = yes
+POINTING_DEVICE_DRIVER = cirque_pinnacle_i2c
+WPM_ENABLE = yes
+BACKLIGHT_ENABLE = no
